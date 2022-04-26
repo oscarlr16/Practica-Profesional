a89dae6f5d7a8953754ae14aa99fe65db1b7f3fc
Oscar León Rodríguez <oscarleon.ro16@gmail.com>
2022-04-26 16:28:21 -0500

    Ejemplo para tratar muliples entradas en keras

--- Algoritmos de ML/Keras_multiples_inputs/ejemplo1.py
+++ Algoritmos de ML/Keras_multiples_inputs/ejemplo1.py
@@ -6,7 +6,124 @@ import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = (8, 8)
 
 games_season = pd.read_csv('games_season.csv')
-games_season.head()
+
 
 games_tourney = pd.read_csv('games_tourney.csv')
-games_tourney.head()
+
+from tensorflow.keras.layers import Embedding, Input, Flatten
+from tensorflow.keras.models import Model
+
+# Count the unique number of teams
+n_teams = np.unique(games_season['team_1']).shape[0]
+
+# Create an embedding layer
+team_lookup = Embedding(input_dim=n_teams,
+                        output_dim=1,
+                        input_length=1,
+                        name='Team-Strength')
+
+# Create an input layer for the team ID
+teamid_in = Input(shape=(1, ))
+
+# Lookup the input in the team strength embedding layer
+strength_lookup = team_lookup(teamid_in)
+
+# Flatten the output
+strength_lookup_flat = Flatten()(strength_lookup)
+
+# Combine the operations into a single, re-usable model
+team_strength_model = Model(teamid_in, strength_lookup_flat, name='Team-Strength-Model')
+
+team_strength_model.summary()
+
+from tensorflow.keras.layers import Concatenate, Dense
+
+# Create an Input for each team
+team_in_1 = Input(shape=(1, ), name='Team-1-In')
+team_in_2 = Input(shape=(1, ), name='Team-2-In')
+
+# Create an input for home vs away
+home_in = Input(shape=(1, ), name='Home-In')
+
+# Lookup the team inputs in the team strength model
+team_1_strength = team_strength_model(team_in_1)
+team_2_strength = team_strength_model(team_in_2)
+
+# Combine the team strengths with the home input using a Concatenate layer, 
+# then add a Dense layer
+
+out = Concatenate()([team_1_strength, team_2_strength, home_in])
+out = Dense(1)(out)
+
+# Make a model
+model = Model([team_in_1, team_in_2, home_in], out)
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_absolute_error')
+
+# Fit the model to the games_season dataset
+model.fit([games_season['team_1'], games_season['team_2'], games_season['home']],
+          games_season['score_diff'],
+          epochs=1, verbose=True, validation_split=0.1, batch_size=2048)
+
+# Evaluate the model on the games_touney dataset
+print(model.evaluate([games_tourney['team_1'], games_tourney['team_2'], games_tourney['home']], 
+                      games_tourney['score_diff'], verbose=False))
+
+
+#model.summary()
+
+from tensorflow.keras.utils import plot_model
+
+# Predict
+games_tourney['pred'] = model.predict([games_tourney['team_1'], 
+                                       games_tourney['team_2'], 
+                                       games_tourney['home']])
+
+# Create an input layer with 3 columns
+input_tensor = Input(shape=(3, ))
+
+# Pass it to a Dense layer with 1 unit
+output_tensor = Dense(1)(input_tensor)
+
+# Create a model
+model = Model(input_tensor, output_tensor)
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_absolute_error')
+
+from sklearn.model_selection import train_test_split
+
+games_tourney_train = games_tourney[games_tourney['season'] <= 2010]
+games_tourney_test = games_tourney[games_tourney['season'] > 2010]
+
+# Fit the model
+model.fit(games_tourney_train[['home', 'seed_diff', 'pred']],
+          games_tourney_train['score_diff'],
+          epochs=1,
+          verbose=True);
+
+# Evaluate the model on the games_tourney_test dataset
+print(model.evaluate(games_tourney_test[['home', 'seed_diff', 'pred']],
+                     games_tourney_test['score_diff'],
+                     verbose=True))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
