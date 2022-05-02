bd6aa89d7e24a3c286bd2fc17841d69627809839
Oscar León Rodríguez <oscarleon.ro16@gmail.com>
2022-05-02 14:08:40 -0500

    Se modifico el archivo multi_output

--- Algoritmos de ML/How_to_Develop_Multi_Output.py
+++ Algoritmos de ML/How_to_Develop_Multi_Output.py
@@ -1,23 +1,12 @@
 # linear regression for multioutput regression
-from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 import numpy as np 
-# create datasets
-X, y = make_regression(n_samples=1000, n_features=10, n_informative=5, n_targets=2, random_state=1, noise=0.5)
-# define model
-model = LinearRegression()
-# fit model
-model.fit(X, y)
-# make a prediction
-row = [0.21947749, 0.32948997, 0.81560036, 0.440956, -0.0606303, -0.29257894, -0.2820059, -0.00290545, 0.96402263, 0.04992249]
-yhat = model.predict([row])
-# summarize prediction
-print(yhat[0])
+
 
 #############################################################################################
 #############################################################################################
 
-dataset = np.loadtxt("planta.csv",delimiter=",")
+dataset = np.loadtxt("planta_T.csv",delimiter=",")
 '''
 Calculamos las dimenciones del data set para generar los vetores U (steps) y el vector t
 '''
@@ -69,7 +58,6 @@ yhat_3 = model.predict([row]).T
 ###############
 
 # example of making a prediction with the direct multioutput regression model
-from sklearn.datasets import make_regression
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.svm import LinearSVR
 # define dataset
